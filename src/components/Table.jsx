62fba3298a232355b7f4d9d864be087d51c79a13
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-03-06 07:30:52 +0500

    add

---
+++ src/components/Table.jsx
@@ -0,0 +1,520 @@
+import React from 'react'
+import './Table.css'
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th rowspan="2">Ramazon oyining kuni</th>
+            <th rowspan="2">Hafta kunlari</th>
+            <th rowSpan="2">Mart oyi</th>
+            <th colspan="2">Bomdod</th>
+            <th colspan="2">Peshin</th>
+            <th colspan="2">Asr</th>
+            <th colspan="2">Shom</th>
+            <th colspan="2">Xufton</th>
+          </tr>
+          <tr>
+            <th>Azon</th>
+            <th>Takbir</th>
+            <th>Azon</th>
+            <th>Takbir</th>
+            <th>Azon</th>
+            <th>Takbir</th>
+            <th>Azon</th>
+            <th>Takbir</th>
+            <th>Azon</th>
+            <th>Takbir</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Shanba</td>
+            <td>1</td>
+            <td>5:32</td>
+            <td>6:05</td>
+            <td>12:30</td>
+            <td>12:45</td>
+            <td>16:20</td>
+            <td>16:30</td>
+            <td>18:09</td>
+            <td>18:14</td>
+            <td>21:</td>
+            <td>21:00</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Yakshanba</td>
+            <td>2</td>
+            <td>5:30</td>
+            <td>6:05</td>
+            <td>12:14</td>
+            <td>12:30</td>
+            <td>17:15</td>
+            <td>17:25</td>
+            <td>19:20</td>
+            <td>20:50</td>
+            <td>21:00</td>
+            <td>21:00</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Dushanba</td>
+            <td>3</td>
+            <td>5:29</td>
+            <td>06:00</td>
+            <td>12:30</td>
+            <td>12:45</td>
+            <td>16:25</td>
+            <td>16:35</td>
+            <td>18:11</td>
+            <td>18:16</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Seshanba</td>
+            <td>4</td>
+            <td>05:27</td>
+            <td>06:00</td>
+            <td>12:29</td>
+            <td>12:45</td>
+            <td>16:25</td>
+            <td>16:35</td>
+            <td>18:12</td>
+            <td>18:17</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>5</td>
+            <td>Chorshanba</td>
+            <td>5</td>
+            <td>05:25</td>
+            <td>06:00</td>
+            <td>12:29</td>
+            <td>12:45</td>
+            <td>16:25</td>
+            <td>16:35</td>
+            <td>18:13</td>
+            <td>18:18</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>6</td>
+            <td>Payshanba</td>
+            <td>6</td>
+            <td>05:24</td>
+            <td>5:55</td>
+            <td>12:29</td>
+            <td>12:45</td>
+            <td>16:25</td>
+            <td>16:35</td>
+            <td>18:14</td>
+            <td>18:19</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>7</td>
+            <td>Juma</td>
+            <td>7</td>
+            <td>05:22</td>
+            <td>5:55</td>
+            <td>12:29</td>
+            <td>12:45</td>
+            <td>16:25</td>
+            <td>16:35</td>
+            <td>18:16</td>
+            <td>18:21</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>8</td>
+            <td>Shanba</td>
+            <td>8</td>
+            <td>05:21</td>
+            <td>5:55</td>
+            <td>12:29</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:17</td>
+            <td>18:22</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>9</td>
+            <td>Yakshanba</td>
+            <td>9</td>
+            <td>05:19</td>
+            <td>5:50</td>
+            <td>12:29</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:18</td>
+            <td>18:23</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>10</td>
+            <td>Dushanba</td>
+            <td>10</td>
+            <td>05:18</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:19</td>
+            <td>18:25</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>11</td>
+            <td>Seshanba</td>
+            <td>11</td>
+            <td>05:17</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:20</td>
+            <td>18:25</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>12</td>
+            <td>Chorsanba</td>
+            <td>12</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:21</td>
+            <td>18:26</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>13</td>
+            <td>Payshanba</td>
+            <td>13</td>
+            <td>5:13</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:22</td>
+            <td>18:27</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+
+          <tr>
+            <td>14</td>
+            <td>Juma</td>
+            <td>14</td>
+            <td>5:11</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:24</td>
+            <td>18:30</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>15</td>
+            <td>Shanba</td>
+            <td>15</td>
+            <td>5:10</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:25</td>
+            <td>18:30</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>16</td>
+            <td>Yakshanba</td>
+            <td>16</td>
+            <td>5:08</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:26</td>
+            <td>18:32</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>17</td>
+            <td>Dushanba</td>
+            <td>17</td>
+            <td>5:06</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:27</td>
+            <td>18:32</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>18</td>
+            <td>Seshanba</td>
+            <td>18</td>
+            <td>5:05</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:28</td>
+            <td>18:33</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>19</td>
+            <td>Chorshanba</td>
+            <td>19</td>
+            <td>5:03</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:29</td>
+            <td>18:35</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>20</td>
+            <td>Payshanba</td>
+            <td>20</td>
+            <td>5:01</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:30</td>
+            <td>18:35</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+
+          <tr>
+            <td>21</td>
+            <td>Juma</td>
+            <td>21</td>
+            <td>4:59</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:31</td>
+            <td>18:36</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>22</td>
+            <td>Shanba</td>
+            <td>22</td>
+            <td>4:57</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:32</td>
+            <td>18:37</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>23</td>
+            <td>Yakshanba</td>
+            <td>23</td>
+            <td>4:56</td>
+            <td>05:15</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:33</td>
+            <td>18:40</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>24</td>
+            <td>Dushanba</td>
+            <td>24</td>
+            <td>4:54</td>
+            <td>05:15</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:34</td>
+            <td>18:40</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>25</td>
+            <td>Seshanba</td>
+            <td>25</td>
+            <td>4:52</td>
+            <td>05:15</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:36</td>
+            <td>18:41</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>26</td>
+            <td>Chorshanba</td>
+            <td>26</td>
+            <td>4:50</td>
+            <td>05:15</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:37</td>
+            <td>18:42</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>27</td>
+            <td>Payshanba</td>
+            <td>27</td>
+            <td>4:48</td>
+            <td>05:15</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:38</td>
+            <td>18:43</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+          <tr>
+            <td>28</td>
+            <td>Juma</td>
+            <td>28</td>
+            <td>4:47</td>
+            <td>05:15</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:39</td>
+            <td>18:45</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>29</td>
+            <td>Juma</td>
+            <td>29</td>
+            <td>4:47</td>
+            <td>5:50</td>
+            <td>12:28</td>
+            <td>12:45</td>
+            <td>16:30</td>
+            <td>16:40</td>
+            <td>18:39</td>
+            <td>18:45</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
